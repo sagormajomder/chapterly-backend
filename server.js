957e49cd996091ac0486f9ed7e1d8e1e04aa0cd9
Sagor Majomder <sagormajomder@gmail.com>
2025-11-13 22:43:19 +0600

    update

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     // DB
     const chapterlyDB = client.db('chapterlyDB');
